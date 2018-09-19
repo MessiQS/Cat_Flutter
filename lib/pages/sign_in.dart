fdeeddff11f6435354d122c5c67a7239b577c3dc
Ye Ma <634028187@qq.com>
2018-09-19 15:07:51 +0800

    账户相关

---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Account {
+  Account({
+    this.email,
+    this.password,
+  });
+  // 邮箱
+  final String email;
+  // 密码
+  final String password;
+
+}
+/**
+ * 登录页面
+ */
+class SignIn extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
+
